048b62e866000a24ae2eec016dd318ea845dc5b7
David Alsh <alshdavid@gmail.com>
2024-05-04 14:52:44 +0700

    inital commit

---
+++ client/auth.js
@@ -0,0 +1,19 @@
+function login() {
+  const target = new URL(window.location.origin)
+  target.pathname = '/login'
+  target.state = JSON.stringify({ return_url: window.location.href })
+  window.location.assign(target)
+}
+
+async function logout() {
+  const target = new URL(window.location.origin)
+  target.pathname = '/logout'
+  target.state = JSON.stringify({ return_url: window.location.href })
+  window.location.assign(target)
+}
+
+async function refresh() {
+  const target = new URL(window.location.origin)
+  target.pathname = '/auth/refresh'
+  await fetch(target)
+}
